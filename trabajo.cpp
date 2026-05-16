094baf9f2b77557ebbff95b47761605519ab7a7b
tomenos57 <tomas.nieto.dali@gmail.com>
2026-05-15 23:08:04 -0400

    1

--- trabajo.cpp
+++ trabajo.cpp
@@ -1,18 +1,63 @@
 #include <iostream>
 #include <string>
-using namespace std;
+#include <fstream>
+#include <chrono>
 
-// VArreglos del arreglo dinamico 
+using namespace std;
+using namespace std::chrono;
+
 string* v = new string[4];       
 int cantidad_actual = 0;         
 int capacidad_maxima = 4;        
-double porcentaje_overhead = 0.2; // Cuánto crece el arreglo al llenarse
-int indice_letras[258];          
-int main(){
+double porcentaje_overhead = 0.2; 
+int indice_letras[258];
 
+size_t CalcularMemoriaRAM() {
+    size_t memoria_total = 0;
+
+    memoria_total += sizeof(v) + sizeof(cantidad_actual) + sizeof(capacidad_maxima) + sizeof(porcentaje_overhead);
+    memoria_total += sizeof(indice_letras); 
+
+    memoria_total += capacidad_maxima * sizeof(string);
+
+    for (int i = 0; i < cantidad_actual; i++) {
+        memoria_total += v[i].capacity(); 
+    }
+
+    return memoria_total;
 }
 
-//metodo de arreglo dinamico
+void Actualizar_arreglo_indice_letras() {
+    for (int i = 0; i < 258; i++) indice_letras[i] = cantidad_actual;
+    for (int i = cantidad_actual - 1; i >= 0; i--) {
+        if (!v[i].empty()) {
+            unsigned char letra = v[i][0];
+            indice_letras[letra] = i;
+        }
+    }
+    for (int i = 255; i >= 0; i--) {
+        if (indice_letras[i] == cantidad_actual && i < 255) {
+            indice_letras[i] = indice_letras[i + 1];
+        }
+    }
+}
+
+int BuscarPosicionParaInsertar(string nueva_palabra) {
+    if (cantidad_actual == 0) return 0;
+    unsigned char letra_inicial = nueva_palabra[0];
+    int l = indice_letras[letra_inicial];
+    int r = indice_letras[letra_inicial + 1] - 1;
+
+    if (l > r) return l;
+
+    while (l <= r) {
+        int medio = (l + r) / 2;
+        if (v[medio] >= nueva_palabra) r = medio - 1;
+        else l = medio + 1;
+    }
+    return l;
+}
+
 int Buscar1(string palabra_buscada) {
     char letra_inicial = palabra_buscada[0];
     
@@ -59,48 +104,51 @@ void insertar1(string nueva_palabra) {
     Actualizar_arreglo_indice_letras();
 }
 
-void Eliminar(string palabra) {
-    int posicion = Buscar1(palabra);
+int main(){
+    ifstream archivo("D1.txt"); 
+    string palabra;
 
-    if (posicion != -1) {
-        for (int i = posicion; i <= cantidad_actual - 2; i++) {
-            v[i] = v[i + 1];
-        }
-        cantidad_actual--;
-        
-        Actualizar_arreglo_indice_letras();
+    if (!archivo.is_open()) {
+        cout << "Error: No se pudo abrir el archivo D1.txt" << endl;
+        return 1; 
     }
+
+    cout << "--- CARGANDO DICCIONARIO ---" << endl;
+    
+    auto inicio_carga = high_resolution_clock::now();
+    
+    while (archivo >> palabra) {
+        insertar1(palabra);
+    }
+    
+    auto fin_carga = high_resolution_clock::now();
+    auto duracion_carga = duration_cast<milliseconds>(fin_carga - inicio_carga);
+    archivo.close(); 
+
+    cout << "Total de palabras insertadas: " << cantidad_actual << endl;
+    cout << "Tiempo total de insercion: " << duracion_carga.count() << " ms" << endl;
+    
+    size_t ram_bytes = CalcularMemoriaRAM();
+    cout << "Espacio estimado en RAM: " << ram_bytes << " bytes (" << ram_bytes / 1024.0 << " KB)" << endl;
+    cout << "----------------------------\n" << endl;
+
+    string palabra_a_buscar = "universidad"; 
+    cout << "Buscando la palabra: '" << palabra_a_buscar << "'" << endl;
+
+    auto inicio_busqueda = high_resolution_clock::now();
+    
+    int pos = Buscar1(palabra_a_buscar);
+    
+    auto fin_busqueda = high_resolution_clock::now();
+    auto duracion_busqueda = duration_cast<microseconds>(fin_busqueda - inicio_busqueda);
+
+    if (pos != -1) {
+        cout << "Resultado: Encontrada en el indice [" << pos << "]" << endl;
+    } else {
+        cout << "Resultado: No existe en el diccionario." << endl;
+    }
+    
+    cout << "Tiempo de busqueda: " << duracion_busqueda.count() << " microsegundos." << endl;
+
+    return 0;
 }
-
-void Actualizar_arreglo_indice_letras() {
-    for (int i = 0; i < 258; i++) indice_letras[i] = cantidad_actual;
-    for (int i = cantidad_actual - 1; i >= 0; i--) {
-        if (!v[i].empty()) {
-            unsigned char letra = v[i][0];
-            indice_letras[letra] = i;
-        }
-    }
-    for (int i = 255; i >= 0; i--) {
-        if (indice_letras[i] == cantidad_actual && i < 255) {
-            indice_letras[i] = indice_letras[i + 1];
-        }
-    }
-}
-
-int BuscarPosicionParaInsertar(string nueva_palabra) {
-    if (cantidad_actual == 0) return 0;
-    unsigned char letra_inicial = nueva_palabra[0];
-    int l = indice_letras[letra_inicial];
-    int r = indice_letras[letra_inicial + 1] - 1;
-
-    if (l > r) return l;
-
-    while (l <= r) {
-        int medio = (l + r) / 2;
-        if (v[medio] >= nueva_palabra) r = medio - 1;
-        else l = medio + 1;
-    }
-    return l;
-}
-
-
